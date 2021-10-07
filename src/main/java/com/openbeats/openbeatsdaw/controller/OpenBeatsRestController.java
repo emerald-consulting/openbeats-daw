b17cd263309ecf2e0bbdcc15ff60c78b634e6ba2
Abheejeet <abheejeet24@Gmail.com>
2021-10-06 20:18:09 -0400

    Updating redirection URL for login.

--- src/main/java/com/openbeats/openbeatsdaw/controller/OpenBeatsRestController.java
+++ src/main/java/com/openbeats/openbeatsdaw/controller/OpenBeatsRestController.java
@@ -55,7 +55,7 @@ public class OpenBeatsRestController {
         if (createUser.verify(code)) {
             log.info("User verified Successfully.");
             RedirectView redirectView = new RedirectView();
-            redirectView.setUrl("http://localhost:3000/signup");
+            redirectView.setUrl("http://openbeats-frontend.s3-website.us-east-2.amazonaws.com/login");
             return redirectView;
 
         } else {
