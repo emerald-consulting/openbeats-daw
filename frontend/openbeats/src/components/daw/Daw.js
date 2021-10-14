7124175dc1e1a7dff84dd5258f43bd81d9805267
Shashank Priya <shashan2@buffalo.edu>
2021-10-14 09:28:40 -0400

    added dynamicdiv

--- frontend/openbeats/src/components/daw/Daw.js
+++ frontend/openbeats/src/components/daw/Daw.js
@@ -2,30 +2,55 @@ import React from 'react'
 import LogNavbar from '../logNavbar/LogNavbar'
 import Pianoui from './pianoui/Pianoui'
 import Fileupload from './Fileupload'
+import Dynamicdiv from '../dynamicdiv/Dynamicdiv'
+import { Button, PlayerIcon } from 'react-player-controls'
+
+const PlayerButton = ({ style, children, ...props }) => (
+    <Button
+      style={{
+        appearance: 'none',
+        outline: 'none',
+        border: 'none',
+        borderRadius: 3,
+        background: 'white',
+        color: 'blue',
+        '&:hover': {
+          'color': 'lightblue',
+        },
+        ...style,
+      }}
+      {...props}
+    >
+      {children}
+    </Button>
+  )
 
 const Daw = () => {
     return (
-        <div className="">
+        <div className="" >
             
             <div className="flex flex-col h-screen">
                 <LogNavbar/>
                 <div className="flex flex-row   bg-gr1" style={{height:'60%'}}>
-                    <div className="  bg-gr4 mr-1 " style={{width:'15%'}}></div>
-                    <div className=" bg-gr4 mr-1 " style={{width:'65%'}}>
-                        <div> row
-                            <div>
-                                Track 1
-                            </div>
-                            <div>
-                                waveform
-                            </div>
-                        </div>
+                    <div className="  bg-gr1 mr-1 " style={{width:'15%'}}>
+                        <PlayerIcon.Play className="w-10 ml-5 mt-5" style={{color:'err'}} />
+                        <PlayerIcon.Pause className="w-10 ml-5"/>
+                        <PlayerIcon.Previous className="w-10 ml-5"/>
+                        <PlayerIcon.Next className="w-10 ml-5"/>
+                        <PlayerIcon.SoundOn className="w-10 ml-5"/>
+                        <PlayerIcon.SoundOff className="w-10 ml-5"/>
                     </div>
-                    <div className=" bg-gr4 " style={{width:'20%'}}></div>
+                    <div className=" bg-gr1 mr-1 p-1" style={{width:'65%'}}>
+                        <Dynamicdiv/>
+                    </div>
+                    <div className=" bg-gr2 mb-0.5" style={{width:'20%'}}>
+                        <p className="text-2xl rounded bg-gr2 p-3 " style={{textAlign:'center'}}>Collaborators</p>
+                        <Dynamicdiv/>
+                    </div>
                 </div>
                 <div style={{ borderTop: "4px solid green"}} ></div>
                 <div className="flex flex-row  bg-gr1" style={{height:'40%'}}>
-                    <div className=" mr-1 bg-gr4" style={{width:'40%'}}>
+                    <div className=" mr-1 bg-gr1" style={{width:'40%'}}>
                         <Fileupload/>
                     </div>
                     <div className=" bg-gr1" style={{width:'60%'}}>
---
+++ frontend/openbeats/src/components/dynamicdiv/Dynamicdiv.js
@@ -0,0 +1,49 @@
+import React,{useState} from 'react'
+
+
+const TrackComp = () => {
+    const [rows, setRows]= useState([1])
+    const handleClickR = index => {
+        const list = [...rows];
+        list.splice(index, 1);
+        setRows(list);
+    }
+    const handleClickA = () => {
+        let last=rows.at(-1)
+        last=last+1
+        setRows([...rows, last]);
+    }
+    return(
+        <div style={{overflowY:'scroll', position:'relative',height:'350px'}}>
+            
+        {
+            rows.map((x,i) => {
+                return (
+                    <div id={x} className="flex flex-row mr-1 mb-0.5"  > 
+                    {/* style={{height:'50%',width:'50%'}} */}
+                        <div className=" bg-gr4 p-2 mr-0.5" style={{width:'20%'}}>
+                            Track {i+1}
+                        </div>
+                        <div style={{width:'80%'}} className=" bg-gr4 p-2 mr-1">
+                            waveform {i+1}
+                        </div>
+                        <button onClick={()=>handleClickR(i)} className="rounded bg-gr3 p-2">Remove</button>
+                        
+                    </div>
+                )
+            })
+        }
+        <button onClick={()=>handleClickA()} className="rounded bg-gr3 p-2 mt-0.5">Add</button>
+    </div>
+    )
+}
+
+const Dynamicdiv = () => {
+    return (
+        <div>
+            <TrackComp />
+        </div>
+    )
+}
+
+export default Dynamicdiv
