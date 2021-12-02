a0b6a74e21b7d3a267634c44364ee2afdd2c54bf
RyChrome <ryandils@buffalo.edu>
2021-12-02 00:14:57 -0500

    got it working in docker and create tables on start up in postgrest. next up rerouting endpoints for local and prod

--- api/src/main/java/com/openbeats/openbeatsdaw/Entity/Session.java
+++ api/src/main/java/com/openbeats/openbeatsdaw/Entity/Session.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "session")
+@Table(name = "studio")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- api/src/main/java/com/openbeats/openbeatsdaw/Entity/User.java
+++ api/src/main/java/com/openbeats/openbeatsdaw/Entity/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "artist")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
