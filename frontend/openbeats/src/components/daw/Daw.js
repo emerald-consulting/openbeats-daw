82f92358e8a292d3b47d0e8fc472a315ec3a6cb9
Shashank Priya <shashan2@buffalo.edu>
2021-12-09 14:51:55 -0500

    fix

--- frontend/openbeats/src/components/daw/Daw.js
+++ frontend/openbeats/src/components/daw/Daw.js
@@ -48,10 +48,10 @@ const Daw = () => {
                 </div>
                 <div style={{ borderTop: "4px solid green"}} ></div>
                 <div className="flex flex-row" style={{height:'30%'}}>
-                    <div className=" pr-5 " style={{width:'30%',whiteSpace:"break-spaces"}}>
+                    <div className=" pr-5 pl-10" style={{width:'30%',whiteSpace:"break-spaces"}}>
                         <Drum/>
                     </div>
-                    <div className="pt-5" style={{width:'70%'}}>
+                    <div className="pt-5 pl-10" style={{width:'70%'}}>
                         <Pianoui2/>
                     </div>
 
