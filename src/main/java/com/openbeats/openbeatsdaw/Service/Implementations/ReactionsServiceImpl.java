bd7789bc625011f9efc44068fd94fb2030ef6089
Bhavyasai Survepalli <bhavyasaisurvepalli@Bhavyasais-MBP.lan>
2022-04-06 01:37:08 -0400

    update reaction controller

--- src/main/java/com/openbeats/openbeatsdaw/Service/Implementations/ReactionsServiceImpl.java
+++ src/main/java/com/openbeats/openbeatsdaw/Service/Implementations/ReactionsServiceImpl.java
@@ -56,41 +56,42 @@ public class ReactionsServiceImpl implements ReactionsService {
     @Override
     public Reactions updateReaction(Long postId, Long userId, Boolean isLike) {
         Optional<Reactions> optionalReaction = reactionsRepository.findByPostIdAndUserId(postId, userId);
-        if (!optionalReaction.isPresent() && isLike) {
-            Reactions newReaction = new Reactions();
-            newReaction.setPostId(postId);
-            newReaction.setUserId(userId);
-            newReaction.setIsLike(isLike);
-            newReaction.setIsDislike(!isLike);
-            newReaction.setPostRef(postRepository.findById(postId).get());
-            newReaction.setUserRef(userRepository.findById(userId).get());
-            reactionsRepository.save(newReaction);
-            return newReaction;
+        Reactions reaction = optionalReaction.get();
+        Post post = reaction.getPostRef();
+        if (isLike) {
+            post.setTotalLikes(post.getTotalLikes() + 1);
+            reaction.setIsLike(isLike);
+            reaction.setIsDislike(!isLike);
+
         } else {
-            Reactions reaction = optionalReaction.get();
-            Post post = reaction.getPostRef();
-            if (isLike) {
-                post.setTotalLikes(post.getTotalLikes() + 1);
+            if (post.getTotalLikes() != 0) {
+                post.setTotalLikes(post.getTotalLikes() - 1);
                 reaction.setIsLike(isLike);
                 reaction.setIsDislike(!isLike);
-
-            } else {
-                if (post.getTotalLikes() != 0) {
-                    post.setTotalLikes(post.getTotalLikes() - 1);
-                    reaction.setIsLike(isLike);
-                    reaction.setIsDislike(!isLike);
-                }
             }
-            reaction.setPostRef(post);
-            reactionsRepository.save(reaction);
-            return reaction;
         }
+        reaction.setPostRef(post);
+        reactionsRepository.save(reaction);
+        return reaction;
     }
 
     @Override
     public Reactions getReactionByPostIdAndUserId(Long postId, Long userId) {
         Reactions reaction = reactionsRepository.findByPostIdAndUserId(postId, userId).get();
         return reaction;
+    }
+
+    @Override
+    public Reactions createReaction(Long postId, Long userId) {
+        Reactions newReaction = new Reactions();
+        newReaction.setPostId(postId);
+        newReaction.setUserId(userId);
+        newReaction.setIsLike(false);
+        newReaction.setIsDislike(false);
+        newReaction.setPostRef(postRepository.findById(postId).get());
+        newReaction.setUserRef(userRepository.findById(userId).get());
+        reactionsRepository.saveAndFlush(newReaction);
+        return newReaction;
     }
 
 }
