f1f11de23fcdd1c70d06a1891bdfe02a13e05bff
velpurisrilakshmi <velpurisrilakshmisri@gmail.com>
2022-04-06 19:20:00 -0400

    sample

--- frontend/openbeats/src/components/header/MainHeader.js
+++ frontend/openbeats/src/components/header/MainHeader.js
@@ -107,7 +107,7 @@ const MainHeader = (props) => {
               className=" flex flex-row "
               to={isUserLoggedin ? "/home" : "/"}
             >
-              <img className="mt-1 h-10" src={logo} alt={"logo"} />
+              {/* <img className="mt-1 h-10" src={logo} alt={"logo"} /> */}
               <h1 className="mt-2 ml-2">Open Beats</h1>
             </Link>
           </Typography>
@@ -119,7 +119,7 @@ const MainHeader = (props) => {
             sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
           >
             <Link className=" flex flex-row " to="/dashboard">
-              <img className="mt-1 h-10" src={logo} alt={"logo"} />
+              {/* <img className="mt-1 h-10" src={logo} alt={"logo"} /> */}
               <strong className="mt-2 ml-2">Open Beats</strong>
             </Link>
           </Typography>
