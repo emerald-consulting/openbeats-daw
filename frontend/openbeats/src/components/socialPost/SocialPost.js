96f8420eea43fae7aa8eaecf3b63ab8ec04e88ff
Harman Singh <harmansi@buffalo.edu>
2022-04-07 01:09:31 -0400

    fix for dom refresh on navigating to profile page

--- frontend/openbeats/src/components/socialPost/SocialPost.js
+++ frontend/openbeats/src/components/socialPost/SocialPost.js
@@ -8,8 +8,8 @@ import PlaylistContext from "../../model/playlist-store/playlist-context";
 import soundImg from "../sound.jpeg";
 import Tooltip from "@mui/material/Tooltip";
 import ReactHashtag from "react-hashtag";
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import LikeButton from '../likeButton/LikeButton'
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import LikeButton from "../likeButton/LikeButton";
 const SocialPost = ({ details }) => {
   const [author, setAuthor] = useState();
   const [isLiked, setIsLiked] = useState(false);
@@ -35,19 +35,19 @@ const SocialPost = ({ details }) => {
     setAuthor(res.data);
   };
 
-  const getIsPostLikedByUser = async () => {
-    const res = await axios.get(url + "/isLiked/" + details.postId, {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-        Authorization: "Bearer " + token,
-      },
-    });
-    setIsLiked(res.data);
-  };
+  // const getIsPostLikedByUser = async () => {
+  //   const res = await axios.get(url + "/isLiked/" + details.postId, {
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       "Access-Control-Allow-Headers": "Content-Type",
+  //       "Access-Control-Allow-Origin": "*",
+  //       "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+  //       Authorization: "Bearer " + token,
+  //     },
+  //   });
+  //   setIsLiked(res.data);
+  // };
 
   const likeHandler = async () => {
     const res = await axios.post(url + "/like/" + details.postId, null, {
@@ -60,7 +60,7 @@ const SocialPost = ({ details }) => {
         Authorization: "Bearer " + token,
       },
     });
-    setIsLiked(res.data);
+    // setIsLiked(res.data);
   };
 
   const addToPlaylistHandler = () => {
@@ -101,7 +101,9 @@ const SocialPost = ({ details }) => {
             </div>
           </Card.Header>
 
-          <Card.Text className={classes.description}><ReactHashtag>{details.description}</ReactHashtag></Card.Text>
+          <Card.Text className={classes.description}>
+            <ReactHashtag>{details.description}</ReactHashtag>
+          </Card.Text>
           {details.trackFileName && (
             <Tooltip title="Add to Queue">
               <button
