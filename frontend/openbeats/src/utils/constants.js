127e3b008f9174c438339d1a5e2ec0cfacebb177
Harman Singh <harmansi@buffalo.edu>
2022-04-29 17:48:21 -0400

    added the ability to create DAW sessions from chat

--- frontend/openbeats/src/utils/constants.js
+++ frontend/openbeats/src/utils/constants.js
@@ -1,8 +1,9 @@
 // export const url = 'http://192.168.1.166:5000'
- export const url = "https://api.tryopenbeats.com"
-// export const url = "http://localhost:5010"
+//  export const url = "https://api.tryopenbeats.com"
+export const url = "http://localhost:5010"
 
 // inbox polling time in miliseconds
-export const inboxPolling = 10000
+export const inboxPolling = 10000;
+export const linkIdenfilier = "%$#!link@#$$";
 
 // export const url = "http://10.84.97.145:5000"
