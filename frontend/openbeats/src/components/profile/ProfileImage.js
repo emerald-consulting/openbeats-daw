193a7673173532968f3790d39f104632197ae9fb
Akshata Singh <akshata.as1598@gmail.com>
2022-05-06 01:35:22 -0400

    Updated with ProfileImage avatar

--- frontend/openbeats/src/components/profile/ProfileImage.js
+++ frontend/openbeats/src/components/profile/ProfileImage.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Avatar from "react-avatar-edit";
 import './Profile.module.css';
-//import Avatar from "react-avatar-edit";
+// import Avatar from "react-avatar-edit";
 
 const ProfileImage = (props) => {
   return (
-     <div>
-    {/* // <Avatar
-    //     width={390}
-    //     height={295}
-    //     onCrop={props.onCrop}
-    //     onClose={props.onClose}
-    //     src={props.profileUrl}
-    //   />
-      <img src={props.preview} alt="Preview" /> */}
-    </div>
+    <div>
+    <Avatar
+      width={390}
+      height={295}
+      onCrop={props.onCrop}
+      onClose={props.onClose}
+      src={props.profileUrl}
+    />
+    <img src={props.preview} alt="Preview" />
+  </div>
   );
 };
 
